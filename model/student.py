70ec5e5a8c8b134395945795762be9d9a79342e3
zdelar <bojan.zdelar@gmail.com>
2020-12-01 14:38:19 +0100

    Dodate klase student i visokoskolska_ustanova

---
+++ model/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, ustanova, broj_indeksa, prezime, ime):
+        self.ustanova = ustanova
+        self.broj_indeksa = broj_indeksa
+        self.prezime = prezime
+        self.ime = ime
---
+++ model/visokoskolska_ustanova.py
@@ -0,0 +1,5 @@
+class VisokoskolskaUstanova:
+    def __init__(self, oznaka, naziv, adresa):
+        self.oznaka = oznaka
+        self.naziv = naziv
+        self.adresa = adresa
