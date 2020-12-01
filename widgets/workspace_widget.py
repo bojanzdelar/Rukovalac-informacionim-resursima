bcd4cb199a466bcd7a499cfeb031a4d80a9d692a
zdelar <bojan.zdelar@gmail.com>
2020-12-01 21:54:51 +0100

    Sitne ispravke

--- widgets/workspace_widget.py
+++ widgets/workspace_widget.py
@@ -10,10 +10,8 @@ class WorkspaceWidget(QtWidgets.QWidget):
 
         self.file_name = file_name
         self.main_layout = QtWidgets.QVBoxLayout()
-        self.main_table = None
-        self.create_main_table()
-        self.tab_widget = None
-        self.create_tab_widget()
+        self.main_table = self.create_main_table()
+        self.tab_widget = self.create_tab_widget()
         self.main_layout.addWidget(self.main_table)
         self.main_layout.addWidget(self.tab_widget)
         self.setLayout(self.main_layout)
@@ -25,24 +23,22 @@ class WorkspaceWidget(QtWidgets.QWidget):
         return table
 
     def create_main_table(self):
-        self.main_table = self.create_table()
-
         if self.file_name == "visokoskolske_ustanove.csv":
-            self.model = VisokoskolskaUstanovaModel()
-            self.model.ustanove = VisokoskolskaUstanova.load()
-            self.main_table.setModel(self.model)
-            self.main_table.clicked.connect(self.selected)
-            
+            model = VisokoskolskaUstanovaModel()
+            model.ustanove = VisokoskolskaUstanova.load()       
         elif self.file_name == "student.csv":
-            self.model = StudentModel()
-            self.model.students = Student.load()      
-            self.main_table.setModel(self.model)
-            self.main_table.clicked.connect(self.selected)
+            model = StudentModel()
+            model.students = Student.load()      
+        
+        main_table = self.create_table()
+        main_table.setModel(model)
+        main_table.clicked.connect(self.selected)
+        return main_table
 
     def selected(self, index):
         if self.file_name == "visokoskolske_ustanove.csv":
-            student_model = StudentModel()
             selected_ustanova = self.main_table.model().get_element(index)
+            student_model = StudentModel()
             student_model.students = selected_ustanova.studenti
             studenti = self.create_table(self.tab_widget)
             studenti.setModel(student_model)
@@ -54,9 +50,10 @@ class WorkspaceWidget(QtWidgets.QWidget):
         #     ...
 
     def create_tab_widget(self):
-        self.tab_widget = QtWidgets.QTabWidget(self)
-        self.tab_widget.setTabsClosable(True)
-        self.tab_widget.tabCloseRequested.connect(self.delete_tab)
+        tab_widget = QtWidgets.QTabWidget(self)
+        tab_widget.setTabsClosable(True)
+        tab_widget.tabCloseRequested.connect(self.delete_tab)
+        return tab_widget
         
     def delete_tab(self, index):
         self.tab_widget.removeTab(index)
