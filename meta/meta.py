c6428ffe8b0ede45d26760064d2a7a3578bdfbdd
Bojan Zdelar <bojan.zdelar@gmail.com>
2021-01-14 14:52:46 +0100

    Izmena strukture meta podataka

--- meta/meta.py
+++ meta/meta.py
@@ -5,3 +5,18 @@ def read_meta():
     path = read_config()["meta"]
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
+
+def get_file_meta(file_name):
+    for item in read_meta().values():
+        if "files" in item and file_name in item["files"]:
+            return item
+
+def get_file_display(file_name):
+    meta = get_file_meta(file_name)
+    return meta["files"][file_name] if meta else ""
+
+def file_in_meta(file_name):
+    for item in read_meta().values():
+        if "files" in item and file_name in item["files"]:
+            return True
+    return False
