8c1819b6a445ead261fcc8d470e4e3cd9266cad5
Bojan Zdelar <bojan.zdelar@gmail.com>
2021-01-21 22:03:53 +0100

    Ispravka greske

--- dialog/dialog.py
+++ dialog/dialog.py
@@ -44,7 +44,7 @@ class Dialog(QtWidgets.QDialog):
                 char_width = QtGui.QFontMetrics(input.font()).averageCharWidth()
                 input.setMaximumWidth(attribute["length"] * char_width + 10)
                 if attribute["input"] == "characters":
-                    input.setValidator(QtGui.QRegExpValidator(".{%s}" % attribute["length"]))
+                    input.setValidator(QtGui.QRegExpValidator(".{%s}|" % attribute["length"]))
                 elif attribute["input"] == "number":
                     input.setValidator(QtGui.QRegExpValidator("[1-9][0-9]*"))
             elif attribute["input"] == "date":
